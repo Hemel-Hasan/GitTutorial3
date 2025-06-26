8ab40edbe3e6357998ee7f37185a98edf14af6d9
Hemel <hemelhasan2084@gmail.com>
2025-06-26 14:30:04 +0600

    new commit master

--- test1.py
+++ test1.py
@@ -1,7 +1,4 @@
 print("Commit No 1")
 print("Commit 'Feature1' No 2" )
-<<<<<<< feature1
 print("This is totally new commit" )
-=======
-print("This is totally new commit part 1" )
->>>>>>> master
+
